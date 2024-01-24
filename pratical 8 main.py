7353a1ad3562ead5370cacc7ee2fbf9d773660be
manikantaL16 <157564962+manikantaL16@users.noreply.github.com>
2024-01-24 11:45:20 +0530

    pratical 8 main.py

---
+++ pratical 8 main.py
@@ -0,0 +1,9 @@
+i=[56,4,2,24,54,32,42,1,10,7] 
+ print('before ascending sort') 
+ print(i) 
+ print('after ascending sort') 
+ i.sort() 
+ print(i) 
+ print('after descending sort') 
+ i .sort(reverse=True) 
+ print(i) 
