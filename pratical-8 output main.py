5e9fdb112a396a06282ef68146512c6c9b0dd91b
manikantaL16 <157564962+manikantaL16@users.noreply.github.com>
2024-01-24 11:45:54 +0530

    pratical-8 code

---
+++ pratical-8 output main.py
@@ -0,0 +1,6 @@
+before ascending sort 
+ [56, 4, 2, 24, 54, 32, 42, 1, 10, 7] 
+ after ascending sort 
+ [1, 2, 4, 7, 10, 24, 32, 42, 54, 56] 
+ after descending sort 
+ [56, 54, 42, 32, 24, 10, 7, 4, 2, 1] 
